e93162ffb6b1ea66b6bf97b0218aedc283fb8e06
Kévin Joya <kevin.joya@outlook.com>
2019-08-03 15:51:14 +0200

    Build code

---
+++ docs/precache-manifest.3923427bc662e6134043b292be113492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389ee1942fd85c10143db1dd1ea26829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cd253a7075c2a78d737",
+    "url": "/static/css/main.ef24283b.chunk.css"
+  },
+  {
+    "revision": "8d9e16bcfb8122475e27",
+    "url": "/static/js/2.c7627d46.chunk.js"
+  },
+  {
+    "revision": "9cd253a7075c2a78d737",
+    "url": "/static/js/main.fd69871b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
